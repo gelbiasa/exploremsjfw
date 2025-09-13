c8e9959dac35d676c754c0b76a15632be3c2becf
Hanief00 <hanifadaadaaja@gmail.com>
2025-09-13 22:00:39 +0700

    create controller for dashboard ppic

--- app/Http/Controllers/DashboardPPICController.php
+++ app/Http/Controllers/DashboardPPICController.php
@@ -8,6 +8,16 @@ class DashboardPPICController extends Controller
 {
     public function index($data)
     {
+        // Ambil total seluruh transaksi upload BOM
+        $totalTransaksi = \App\Models\Transaksi\TrsBomHModel::count();
+
+        // Hitung jumlah BOM Jumbo berdasarkan material_fg_sfg karakter pertama bukan angka (A-Z/a-z)
+        $totalJumbo = \App\Models\Transaksi\TrsBomHModel::whereRaw("LEFT(material_fg_sfg, 1) REGEXP '^[A-Za-z]'")
+            ->count();
+
+        // Kirim data ke view
+        $data['totalTransaksi'] = $totalTransaksi;
+            $data['totalJumbo'] = $totalJumbo;
         return view($data['url'], $data);
     }
 }
